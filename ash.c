b7d347c3eea3a610b38d2dbb8e56dc9e88e20061
abdurehman4 <arehmanimran4@gmail.com>
2025-10-24 11:36:44 +0500

    Added fork() and execvp()

--- ash.c
+++ ash.c
@@ -1,21 +1,32 @@
+#include "tokenizer.h"
 #include <linux/limits.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 void getCommand(char **string);
 void prompt(int *isFirst, char *path);
+int newProcess(tokens_t *tokens);
 
 int main() {
   char path[PATH_MAX];
+  tokens_t *tokens;
   char *command;
   int isFirst = 1;
-
   getcwd(path, sizeof(path));
   while (1) {
     prompt(&isFirst, path);
     getCommand(&command);
-    printf("The given cmd is :\n%s", command);
+    if (strlen(command) == 0) {
+      continue;
+    }
+    tokenize(command, &tokens);
+    if (strcmp(tokens->array[0], "exit") == 0) {
+      return 0;
+    }
+    newProcess(tokens);
   }
 }
 
@@ -30,4 +41,24 @@ void getCommand(char **string) {
   fgets(buffer, 1024, stdin);
   buffer[strcspn(buffer, "\n")] = '\0';
   *string = strdup(buffer);
+}
+
+int newProcess(tokens_t *tokens) {
+  pid_t pid;
+  pid = fork();
+  int status;
+  char **args = tokens->array;
+  if (pid == 0) {
+    if (execvp(args[0], args) == -1) {
+      perror("");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    perror("Error occured while forking");
+  } else {
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
 }
